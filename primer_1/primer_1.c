f783a608c182789715e8b12d19bfee5492535df5
minic98 <mstefan98@gmail.com>
2026-02-05 20:53:44 +0100

    This is actual first example from 2-1 - OpeMP - primeri.pdf

--- primer_1/primer_1.c
+++ primer_1/primer_1.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
+#include <omp.h>
 
 int main() {
-    printf("Hello World \n");
+    printf("Hello World: ");
+#pragma omp parallel
+    printf("%d ", omp_get_thread_num());
+    printf("\n");
+    return 0;
 }
