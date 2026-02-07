330aba251970fd52e7dcb52c5211ae9f44a845ca
minic98 <mstefan98@gmail.com>
2026-02-07 18:06:49 +0100

    Fixed formatting in primer_3_1; Added exe file.

--- primer_3_1/primer_3_1.c
+++ primer_3_1/primer_3_1.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #include <omp.h>
 
-int main() {
-    printf("Hello World");
+// Paralelizacija petlji
+
+int main(int argc, char* argv[]) {
+    int max;
+
+    printf("Unesite maksimalnu vrednost: ");
+    scanf("%d", &max);
+
+        #pragma omp parallel for
+        for(int i=0; i<max; i++) {
+            printf("Thread: %d, Index: %d \n", omp_get_thread_num(), i);
+        }
+
     return 0;
 }
